bcc9c2559111b0ce4fc1755b2dfc9d0c06ad853e
Layz130 <109735432+Layz130@users.noreply.github.com>
2024-12-05 17:06:45 +0000

    la

--- bd.js
+++ bd.js
@@ -14,4 +14,25 @@ async function selectUsuarios() {
     const res = await client.query("SELECT * FROM usuario");
     return res.rows;
   }
-export { selectUsuarios };
+
+//bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  client.release();
+  return res.rows;
+}
+//bd.js
+
+//bd.js
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+  client.release();
+}
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 
 dotenv.config();
 
@@ -31,3 +31,20 @@ app.listen(port, () => {
   // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
+  //index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario/# solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+//index.js
+app.use(express.json());
+//index.js
+
+
